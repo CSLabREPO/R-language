0e28ceb6acc35f7148ede4a15694c7ba84ddcfd1
CSLabREPO <52801567+CSLabREPO@users.noreply.github.com>
2019-07-11 17:15:15 -0500

    Create basic_commands.R

---
+++ R-PRIMEROS-COMANDOS/basic_commands.R
@@ -0,0 +1,32 @@
+#---------------------------------------------------------------# 
+#   https://computers-science-lab.blogspot.com/search/label/R   #
+#                   { "DEV":"Javi Muro"}                        #
+#---------------------------------------------------------------#
+
+#POINT A VALUE TO A VARIABLE
+#This command is basic, there are 2 ways to do it 
+
+#First way 
+  VarStr="String"
+  VarInt=2
+
+print(VarStr)
+print(VarInt)
+
+#Second way
+  VarStr<-"string"
+  VarInt<-34
+
+print(VarStr)
+print(VarInt)
+
+#Standar deviation
+#This command execute the standar deviation formula,it only need 3 parameters: command(c(PARAMETER1,PARAMETER 2,PARAMETER N...));
+
+  sd(c(1, 2, 3, 4, 5, 6))
+
+
+
+
+
+licence()
