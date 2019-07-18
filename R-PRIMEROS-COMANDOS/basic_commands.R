a42113ba04d8c9d5a0367cb79f5a5b04efa58d29
CSLabREPO <52801567+CSLabREPO@users.noreply.github.com>
2019-07-18 17:05:03 -0500

    Update basic_commands.R

--- R-PRIMEROS-COMANDOS/basic_commands.R
+++ R-PRIMEROS-COMANDOS/basic_commands.R
@@ -4,29 +4,41 @@
 #---------------------------------------------------------------#
 
 #POINT A VALUE TO A VARIABLE
-#This command is basic, there are 2 ways to do it 
+#This command is basic, there are 2 ways to
 
 #First way 
   VarStr="String"
   VarInt=2
 
-print(VarStr)
-print(VarInt)
+    print(VarStr)
+    print(VarInt)
 
 #Second way
   VarStr<-"string"
   VarInt<-34
-
-print(VarStr)
-print(VarInt)
+  
+    print(VarStr)
+    print(VarInt)
 
 #Standar deviation
-#This command execute the standar deviation formula,it only need 3 parameters: command(c(PARAMETER1,PARAMETER 2,PARAMETER N...));
+#This command execute the standar deviation formula,
+#it only need 3 parameters: command(c(PARAMETER1,PARAMETER 2,PARAMETER N...));
 
   sd(c(1, 2, 3, 4, 5, 6))
 
+#Vectors
+  
+#this command allows you to create a vector where you can point different values
+#the sintax command is= variable = c(..., ..., ...);
 
+  Var2=c(1,2,3,4,5,6,7)
 
+#with this command you can check the number of elements in the vector
 
+  length(Var2)
+#if you want to create a vector, you can use the operator : 
 
-licence()
+  Var2<-1:16
+
+  print(Var2)
+#Lists
